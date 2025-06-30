18117db08befc9eb89c25c83fb19a5142f84ad6e
Akhil <akhilkrish987@gmail.com>
2025-06-30 10:11:10 +0530

    gen

--- main.tf
+++ main.tf
@@ -2,18 +2,39 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.8.0"
+      version = "~> 6.8"
     }
   }
 }
 
 provider "google" {
-  project = "newterraformvariables"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file)
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_network" "default" {
+  name = "default-network"
 }
+
+resource "google_compute_instance" "default" {
+  name         = var.new
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.boot_disk_image
+      size  = var.boot_disk_size_gb
+      type  = var.boot_disk_type
+    }
+    auto_delete = var.boot_disk_auto_delete
+  }
+
+  network_interface {
+    network = google_compute_network.default.id
+    access_config {}  # Needed for external IP (optional)
+  }
+
 
