415570436f8d28ac7694aeff8148c4d5b0d3e908
Akhilgit911 <akhilgcp726@gmail.com>
2025-06-30 15:48:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,49 @@
-variable "project" {}
-variable "region" {}
-variable "zone" {}
-variable "credentials_file" {}
-variable "new" {}
-variable "machine_type" {}
-variable "boot_disk_image" {}
-variable "boot_disk_size_gb" {}
-variable "boot_disk_type" {}
-variable "boot_disk_auto_delete" {}
+variable "project" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Path to credentials file"
+  type        = string
+}
+
+variable "new" {
+  description = "Instance name"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Instance machine type"
+  type        = string
+}
+
+variable "boot_disk_image" {
+  description = "Boot disk image"
+  type        = string
+}
+
+variable "boot_disk_size_gb" {
+  description = "Boot disk size in GB"
+  type        = number
+}
+
+variable "boot_disk_type" {
+  description = "Boot disk type"
+  type        = string
+}
+
+variable "boot_disk_auto_delete" {
+  description = "Auto delete boot disk"
+  type        = bool
+}
