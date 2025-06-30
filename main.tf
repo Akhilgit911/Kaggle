bc14458bb633986660a7b7279d57af762baf08ed
Akhil <akhilkrish987@gmail.com>
2025-06-30 16:58:33 +0530

    newfile

--- main.tf
+++ main.tf
@@ -8,33 +8,10 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
-  credentials = file(var.credentials_file)
+  project = "newterraformvariables"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
-
-resource "google_compute_network" "default" {
-  name = "default-network"
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
-
-resource "google_compute_instance" "default" {
-  name         = var.new
-  machine_type = var.machine_type
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = var.boot_disk_image
-      size  = var.boot_disk_size_gb
-      type  = var.boot_disk_type
-    }
-    auto_delete = var.boot_disk_auto_delete
-  }
-
-  network_interface {
-    network = google_compute_network.default.id
-    access_config {} # Needed for external IP (optional)
-  }
-}
-
